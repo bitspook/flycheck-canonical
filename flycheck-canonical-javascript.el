4b34d4550eed085563a93bf7b5335bb0757bffff
Charanjit Singh <ckhabra007@gmail.com>
2015-11-27 01:49:18 +0530

    Typo

--- flycheck-canonical-javascript.el
+++ flycheck-canonical-javascript.el
@@ -4,7 +4,7 @@
       "A javascript syntax and style checker using the Canonical tool.
 
        See URL `https://github.com/gajus/canonical'."
-      :command ("node" "/Users/channi/Documents/Work/applaudience/canonical/dist/bin/index.js" "--stdin" "--linter" "js" "--outputFormat" "checkstyle" "--filePath" source-original)
+      :command ("canonical" "--stdin" "--linter" "js" "--outputFormat" "checkstyle" "--filePath" source-original)
       :standard-input t
       :error-parser flycheck-parse-checkstyle
       :modes (js-mode js2-mode js3-mode))
