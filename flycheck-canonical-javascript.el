caf91399fa9e878b878aaf2168428b57afd7f8b4
Charanjit Singh <ckhabra007@gmail.com>
2015-11-27 09:20:54 +0530

    Define canoical checkers but don't load them automatically

--- flycheck-canonical-javascript.el
+++ flycheck-canonical-javascript.el
@@ -1,14 +1,13 @@
 (after-load 'flycheck
-  (progn
-    (flycheck-define-checker canonical-javascript
-      "A javascript syntax and style checker using the Canonical tool.
+  (flycheck-define-checker canonical-javascript
+    "A javascript syntax and style checker using the Canonical tool.
 
        See URL `https://github.com/gajus/canonical'."
-      :command ("canonical" "--stdin" "--linter" "js" "--outputFormat" "checkstyle" "--filePath" source-original)
-      :standard-input t
-      :error-parser flycheck-parse-checkstyle
-      :modes (js-mode js2-mode js3-mode))
+    :command ("canonical" "--stdin" "--linter" "js" "--outputFormat" "checkstyle" "--filePath" source-original)
+    :standard-input t
+    :error-parser flycheck-parse-checkstyle
+    :modes (js-mode js2-mode js3-mode)))
 
-    (add-to-list 'flycheck-checkers 'canonical-javascript)))
+;; (add-to-list 'flycheck-checkers 'canonical-javascript)
 
 (provide 'flycheck-canonical-javascript)
--- flycheck-canonical-scss.el
+++ flycheck-canonical-scss.el
@@ -1,14 +1,13 @@
 (after-load 'flycheck
-  (progn
-    (flycheck-define-checker canonical-scss
-      "A javascript syntax and style checker using the Canonical tool.
+  (flycheck-define-checker canonical-scss
+    "A javascript syntax and style checker using the Canonical tool.
 
        See URL `https://github.com/gajus/canonical'."
-      :command ("canonical" "--stdin" "--linter" "scss" "--outputFormat" "checkstyle" "--filePath" source-original)
-      :standard-input t
-      :error-parser flycheck-parse-checkstyle
-      :modes (scss-mode sass-mode css-mode))
+    :command ("canonical" "--stdin" "--linter" "scss" "--outputFormat" "checkstyle" "--filePath" source-original)
+    :standard-input t
+    :error-parser flycheck-parse-checkstyle
+    :modes (scss-mode sass-mode css-mode)))
 
-    (add-to-list 'flycheck-checkers 'canonical-scss)))
+;; (add-to-list 'flycheck-checkers 'canonical-scss)
 
 (provide 'flycheck-canonical-scss)
